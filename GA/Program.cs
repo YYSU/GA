ba72744ae5cafe2b1eb1a4163bd5c5a6f0a01002
YYSU <s102213004@mail1.ncnu.edu.tw>
2016-06-09 23:26:45 +0800

    增加 算術交配法

--- GA/Program.cs
+++ GA/Program.cs
@@ -37,6 +37,8 @@ namespace GA
             for (int i = 0; i < PopulationSize.Length; i++) {
                 Fitness[i] = fitness(key, PopulationSize[i]);
             }
+            // 排序fitness
+            bubbleSort(PopulationSize, Fitness);
         }
 
         // 2進位要補滿的位置
@@ -89,9 +91,6 @@ namespace GA
 
         // 輪盤法
         public void Roulette_Wheel() {
-            // 排序fitness
-            bubbleSort(PopulationSize, Fitness);
-
             //開始進行天擇，分數越高的選到機率越高
             bool[] arr = new bool[17];
             for (int i = 0; i < 17; i++) {
@@ -119,7 +118,7 @@ namespace GA
                 }
             }
 
-            //總數
+            //有出現過的Fitness,加到list
             ArrayList list = new ArrayList();
             int total = 0;
             for (int i = 0; i < 17; i++) {
@@ -172,6 +171,7 @@ namespace GA
                 }
 
                 conserve = PopulationSize[value.Next(start, end)];
+               // Console.WriteLine("start:{0} ,end:{1},value:{2}", start, end,fitness(key,conserve));
                 NewPopulation[i] = conserve;       
             }
 
@@ -187,7 +187,7 @@ namespace GA
             bubbleSort(PopulationSize, Fitness);
         }
 
-        // 交配
+        // 單點交配
         public void One_Point(double PC, double MR) {
             double MutateRate;
             for (int i = 0; i < PopulationSize.Length; i++) {
@@ -197,6 +197,7 @@ namespace GA
                 chromosome[1] = PopulationSize[value.Next(0, PopulationSize.Length)];
                 //chromosome[0] = Choice();
                 //chromosome[1] = Choice();
+               
 
                 if (MutateRate < MR)
                 {
@@ -227,13 +228,71 @@ namespace GA
             
             Translate();
             bubbleSort(PopulationSize, Fitness);
-
-            int sum = 0;
-            for (int i = 0; i < PopulationSize.Length; i++) {
-                sum += Fitness[i];
-            }
-            Console.WriteLine("平均: {0}", sum / PopulationSize.Length);
         }
+
+        // 算數交配
+        public void Arithmetic(double PC, double MR)
+        {
+            double MutateRate;
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < PopulationSize.Length; i++)
+            {
+                MutateRate = value.Next(0, 1);
+                // 任取人口數中兩個
+                chromosome[0] = PopulationSize[value.Next(0, PopulationSize.Length)];
+                chromosome[1] = PopulationSize[value.Next(0, PopulationSize.Length)];
+                //chromosome[0] = Choice();
+                //chromosome[1] = Choice();
+
+
+                if (MutateRate < MR)
+                {
+                    //算術交配，做 And 運算，(1 && 1 = 1),其他得0
+                    for (int j = 0; j < chromosome[0].Length; j++) {
+                        if (chromosome[0][j] == '1')
+                        {
+                            if (chromosome[1][j] == '1')
+                            {
+                                str.Append("1");
+                            }
+                            else
+                            {
+                                str.Append("0");
+                            }
+                        }
+                        else {
+                            str.Append("0");
+                        }
+                    }
+
+                    NewChromosome = str.ToString();
+                    str.Clear();
+                    int Probability = value.Next(0, 1);
+                    // 突變
+                    if (Probability < PC)
+                        NewChromosome = Mutate(NewChromosome);
+                    NewPopulation[i] = NewChromosome;
+                }
+                else
+                {
+                    NewChromosome = fitness(key, chromosome[0]) > fitness(key, chromosome[1]) ? chromosome[0] : chromosome[1];
+                    int Probability = value.Next(0, 1);
+                    // 突變
+                    if (Probability < PC)
+                        NewChromosome = Mutate(NewChromosome);
+                    NewPopulation[i] = NewChromosome;
+                }
+            }
+
+            for (int i = 0; i < NewPopulation.Length; i++)
+            {
+                NewFitness[i] = fitness(key, NewPopulation[i]);
+            }
+
+            Translate();
+            bubbleSort(PopulationSize, Fitness);
+        }
+
 
         // 突變
         public String Mutate(String Chromosome) {
@@ -260,7 +319,7 @@ namespace GA
 
         public String Choice()
         {
-            //開始進行天擇，分數越高的選到機率越高
+            //分數越高的選到機率越高
             bool[] arr = new bool[17];
             for (int i = 0; i < 17; i++)
             {
@@ -358,6 +417,23 @@ namespace GA
             for (int i = 0; i < PopulationSize.Length; i++) {
                 Console.WriteLine("第{0}個 : {1}  , Fitness:{2},{3}", i+1, PopulationSize[i], Fitness[i], fitness(key, PopulationSize[i]));
             }
+
+            int sum = 0;
+            double Avg, SD = 0;
+
+            for (int i = 0; i < PopulationSize.Length; i++)
+            {
+                sum += Fitness[i];
+            }
+            Avg = sum / PopulationSize.Length;
+
+            for (int i = 0; i < PopulationSize.Length; i++)
+            {
+                SD += (Fitness[i] - Avg) * (Fitness[i] - Avg);
+            }
+            SD = Math.Sqrt(SD/PopulationSize.Length);
+             
+            Console.WriteLine("平均: {0}, 標準差:{1}", Avg, SD);
             Console.WriteLine();
         }
     }
@@ -373,11 +449,11 @@ namespace GA
 
             // 人口數
             GA ga = new GA(100, key);
-            
-            // 執行次數，突變率,  交配率
-            int RunTime = 40;
-            double PC = 0.3;
-            double MR = 0.9;
+            ga.Print();
+            // 執行次數，突變率, 交配率
+            int RunTime = 100;
+            double PC = 0.1;
+            double MR = 0.8;
         
             for (int i = 0; i < RunTime; i++) {
                 Console.WriteLine("這是第 {0} 次 ", times+1);
